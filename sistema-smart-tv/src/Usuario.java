bb00fa71c3809660a7f3c8180aa41d6718092178
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-06-14 11:09:34 -0300

    Finalizado o módulo sobre métodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        smartTv.aumentarVolume();
+        smartTv.canalAnterior();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(10);
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+
+    }
+}
